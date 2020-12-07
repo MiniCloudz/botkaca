ef22f6dd293c98d23184e8130710e98059eeca88
MaxxRepoHub <60533346+MaxxRepoHub@users.noreply.github.com>
2020-12-07 11:31:35 +0530

    Update thumbnail_handler.py

--- bot/handlers/thumbnail_handler.py
+++ bot/handlers/thumbnail_handler.py
@@ -1,23 +1,30 @@
+
 from pyrogram import Client, Message, Filters
 from os.path import join as os_path_join
 from bot import COMMAND, LOCAL, CONFIG
 from bot.plugins import thumbnail_video
-
-thumbnail_path = os_path_join(CONFIG.ROOT, CONFIG.WORKDIR, CONFIG.THUMBNAIL_NAME)
-
+ 
 @Client.on_message(Filters.command("savethumb") & Filters.incoming)
 async def set(client, message):
     if message.reply_to_message is not None:
         name = str(message.from_user.id) + "/" + ".jpg"
-        thumbnail_path = os_path_join(CONFIG.ROOT, CONFIG.WORKDIR, name
-    )
-    await reply.edit_text(LOCAL.THUMBNAIL_DOWNLOADED)
-    await thumbnail_video.set(thumbnail_path)
-    await reply.edit_text(LOCAL.THUMBNAIL_APPLIED)
-
+        thumbnail_path = os_path_join(CONFIG.ROOT, CONFIG.WORKDIR, name)
+ 
+        await message.reply_to_message.download(
+            file_name = thumbnail_path
+        )
+        await thumbnail_video.set(thumbnail_path)
+        await message.reply_text("✅ Successfully saved thumbnail", quote=True)
+    else:
+        await m.reply_text(text="Reply to a photo to set custom thumbnail")
+ 
 @Client.on_message(Filters.command(COMMAND.RESET_THUMBNAIL))
 async def reset(client : Client, message: Message):
-    reply = await message.reply_text(LOCAL.THUMBNAIL_DELETING)
-    await thumbnail_video.reset(thumbnail_path)
-    await reply.edit_text(LOCAL.THUMBNAIL_RESET)
-
+ 
+    name = str(message.from_user.id) + "/" + ".jpg"
+    thumbnail_path = os_path_join(CONFIG.ROOT, CONFIG.WORKDIR, name)
+    if os_path_join.exists(thumbnail_path):
+       await thumbnail_video.reset(thumbnail_path)
+       await message.reply_text("✅ Thumbnail Deleted successfully", quote=True)
+    else:
+      await message.reply_text("No thumbnail found 🤷‍♂️")
